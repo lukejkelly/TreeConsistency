42696aecae6e5445802242f42ad2a6b78cf17776
Luke Kelly <kelly.luke.joseph@gmail.com>
2023-06-12 09:44:32 +0100

    Work in progress

--- R/generate-uniform.R
+++ R/generate-uniform.R
@@ -1,58 +1,44 @@
 # sequentially build unrooted trees with n = n_seq leaves with a uniform
 # distribution across topologies and exponential branch lengths then generate
-# coupled sequences at K = max(k_seq) sites from a JC69 model with mutation
-# rates m_seq
+# sequences at K sites from a JC69 model with mutation rates m_seq
 
 source("pars.R")
 source(file.path("R", "generate-utilities.R"))
+s <- "uniform"
 
 n <- min(n_seq)
 tree <- ape::rtree(n, rooted = FALSE, br = rexp)
-write_tree(tree, "uniform")
+write_tree(tree, s)
 
-alleles <- vector(mode = "list", length = length(m_seq))
 K <- max(k_seq)
-for (j in seq_along(m_seq)) {
-    mu <- m_seq[j]
-    alleles[[j]] <-
-        phangorn::simSeq(
-            tree,
-            l = K,
-            type = "USER",
-            levels = c("0", "1"),
-            rate = mu
-        ) |>
-        as.data.frame()
-    write_alleles(alleles[[j]], "uniform", mu)
+for (m in m_seq) {
+    alleles <- simulate_alleles(tree, K, m)
+    write_alleles(alleles, s, m)
 }
 
 N <- max(n_seq)
-K <- max(k_seq)
 while (n < N) {
-    i1 <- sample(tree$edge, 1)
-    i2 <- paste0("t", n + 1)
-    x1 <- rexp(1)
-    x2 <- rexp(1)
+    b <- sample.int(nrow(tree$edge), 1)
+    i <- sample(tree$edge[b, ], 1)
+    j <- paste0("t", n + 1)
+    x <- rexp(2)
 
-    tree <- tree |>
-        TreeTools::AddTip(i1, i2, 0, 0) |>
-        extend_leaves(i1, x1) |>
-        extend_leaves(i2, x2)
-    write_tree(tree, "uniform")
+    if (i <= n) {
+        tree <- tree |>
+            extend_branch(b, x[1]) |>
+            TreeTools::AddTip(i, j, x[2], x[1])
+    } else {
+        p <- tree$edge[tree$edge[, 2] == i, 1]
+        tree <- tree |>
+            extend_branch()
 
-    for (j in seq_along(m_seq)) {
-        mu <- m_seq[j]
-        alleles <-
-            phangorn::simSeq(
-                tree,
-                l = K,
-                type = "USER",
-                levels = c("0", "1"),
-                rate = mu,
-                ancestral = TRUE
-            ) |>
-            as.data.frame()
-        write_alleles(alleles, "uniform", mu)
+    }
+
+    write_tree(tree, s)
+
+    for (m in m_seq) {
+        alleles <- simulate_alleles(tree, K, m)
+        write_alleles(alleles, s, m)
     }
     n <- n + 1
 }
--- pars.R
+++ pars.R
@@ -1,4 +1,4 @@
 # sequences of n taxa, k sites and mutation rate m
 n_seq <- seq.int(4, 7, 1)
 m_seq <- c(0.1)
-k_seq <- 10^seq.int(0, 2)
+k_seq <- 10^seq.int(0, 4)

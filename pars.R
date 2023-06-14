ee82045fa9674d174e79de3dfa762f96c3d92aab
Luke Kelly <kelly.luke.joseph@gmail.com>
2023-06-14 17:41:23 +0100

    Simulation settings

--- pars.R
+++ pars.R
@@ -1,5 +1,5 @@
-# sequences of: no. taxa n, mutation rate m, no. sites k, replicate index r
-n_seq <- seq.int(4, 10, 3)
-m_seq <- c(0.05, 0.1, 0.2)
-k_seq <- 10^seq.int(0, 3)
-r_seq <- seq_len(5)
+# sequences of taxa n, mutation rate m, sites k, replicate index r
+n_seq <- seq.int(4, 25, 3)
+m_seq <- c(0.1, 0.2, 0.4)
+k_seq <- 10^seq.int(0, 6)
+r_seq <- seq_len(10)

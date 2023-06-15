36c3455b003de6407f9d60726ee87225f7b532dd
Luke Kelly <kelly.luke.joseph@gmail.com>
2023-06-15 18:42:07 +0100

    Remove Bactrian proposal, increase burn-in and run length

--- pars.R
+++ pars.R
@@ -1,5 +1,5 @@
 # sequences of taxa n, mutation rate m, sites k, replicate index r
-n_seq <- seq.int(4, 25, 3)
+n_seq <- seq.int(4, 16, 3)
 m_seq <- c(0.1, 0.2, 0.4)
-k_seq <- 10^seq.int(0, 6)
+k_seq <- 10^seq.int(0, 5)
 r_seq <- seq_len(10)

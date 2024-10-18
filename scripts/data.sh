61433ce1d90f35d3b6db82e86d02b9db7cf7644f
Luke Kelly <kelly.luke.joseph@gmail.com>
2024-10-18 16:29:57 +0100

    Create data folders via separate commands

--- scripts/data.sh
+++ scripts/data.sh
@@ -1,4 +1,4 @@
 # sample finite sites JC69 alleles then process into subsequences
-mkdir -p data/{raw,proc}
+mkdir -p data/raw data/proc
 R -f code/data/generate-data.R
 R -f code/data/process-data.R
